1507e947eb828ff3587eefefccd82c884444449f
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-02-02 11:59:47 +0000

    fix

--- replace.sh
+++ replace.sh
@@ -2,10 +2,14 @@
 
 result=$(cat << EOS
 <!-- start -->
-$(grep desc files/*.yaml | awk -F':desc:' '{printf "- %s\t%s\n",$1,$2}')
+$(grep desc files/*.yaml | awk -F':desc:' '{print "-",$1,"\t",$2}')
 <!-- end -->
 EOS
 )
 
-sed -i -e 's/<!-- start -->.*<!-- end -->/"$result"/g' README.md
+#echo "$result"
+
+foo="ok"
+
+awk '/<!-- start -->/{print '${foo}'} /<!-- start -->/,/<!-- end -->/{next}1' README.md
 
